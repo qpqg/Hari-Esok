01d7746f9a76e54624335064971e61f16cb2f6f3
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2020-12-30 07:56:08 -0800

    Update hari.py

--- hari.py
+++ hari.py
@@ -2,7 +2,7 @@ def uwu(hari):
     hari_hari = ["senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"]
     if hari in hari_hari:
         index_hari = hari_hari.index(hari) + 1
-        if index_hari >= 7:
+        if index_hari >= len(hari_hari):
             index_hari = 0
         print(f"Hari ini, Hari {hari.upper()}. Esok hari {hari_hari[index_hari].upper()}")
     else:
@@ -10,4 +10,3 @@ def uwu(hari):
 
 while True:
     ask = uwu(input("Masukkan Hari (huruf kecil): "))
-
