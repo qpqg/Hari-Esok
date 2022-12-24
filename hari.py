c45e2e4adb98097a1afaa755133e1944cae1cba3
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2022-12-24 15:54:48 +0800

    Update hari.py

--- hari.py
+++ hari.py
@@ -4,9 +4,10 @@ def uwu(hari):
         index_hari = hari_hari.index(hari) + 1
         if index_hari >= len(hari_hari):
             index_hari = 0
-        print(f"Hari ini, Hari {hari.upper()}. Esok hari {hari_hari[index_hari].upper()}")
+        return f"Hari ini, Hari {hari.upper()}. Esok hari {hari_hari[index_hari].upper()}"
     else:
-        print("Hari-harimu untuk hari esok sudah tidak ada")
+        return "Hari-harimu untuk hari esok sudah tidak ada"
 
 while True:
     ask = uwu(input("Masukkan Hari (huruf kecil): "))
+    print(ask)
